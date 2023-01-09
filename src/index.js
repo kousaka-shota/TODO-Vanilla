9b9c68b9874f383bf54930387c227a52f5ab4ee4
Shota Kousaka <114033759+kousaka-shota@users.noreply.github.com>
2023-01-10 08:11:50 +0900

    set

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import "./styles.css";
 
 const OnclickAdd = () => {
+  //テキストインプットの内容を取得・初期化
   const inputText = document.getElementById("input-text").value;
-  alert(inputText);
   document.getElementById("input-text").value = "";
+
+  //タスクの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // document.getElementsByClassName("imcomplete-area")[0].appendChild(div);
+  document.getElementById("ul-area").appendChild(div);
 };
 
 document.getElementById("add-button").addEventListener("click", () => {
