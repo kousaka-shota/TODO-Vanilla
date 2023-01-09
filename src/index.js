a3c80a6d51cea5e513f1fa6edc6e2454eb927c5b
Shota Kousaka <114033759+kousaka-shota@users.noreply.github.com>
2023-01-10 08:00:12 +0900

    set

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const OnclickAdd = () => {
+  const inputText = document.getElementById("input-text").value;
+  alert(inputText);
+  document.getElementById("input-text").value = "";
+};
+
+document.getElementById("add-button").addEventListener("click", () => {
+  OnclickAdd();
+});
